08a3730508f084bb8bd35b34e0d325d48e4c412f
Aydin Gurbanov <qurbanovaydin423@gmail.com>
2026-05-13 12:49:01 +0400

    Add solution to the exercise 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+        let repeated = string;
+        if (num < 0) {
+            return 'ERROR!'
+        } else { 
+            for (let i = 1; i < num; i++) {
+             repeated += string;
+        }
+        return repeated;
+    }
+}
+;
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
