7cc3ed20a1898f0550a25f11d422de56a44e20af
Aydin Gurbanov <qurbanovaydin423@gmail.com>
2026-05-13 13:34:14 +0400

    Add new solutions to the ex.07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let arr = word.split("");
+    let reversedArray = arr.reverse();
+    let newWord = reversedArray.join("");
+    return newWord;
 };
 
 // Do not edit below this line
