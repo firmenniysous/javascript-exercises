89d5859f0d885572e2a0cd417561faee32377947
Aydin Gurbanov <qurbanovaydin423@gmail.com>
2026-05-14 15:15:32 +0400

    Add the solution for ex.08 that passes all the tests

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...del) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+    for (let arg = del.length; arg >= 0; arg--) {
+        let j = arr[i];
+        let k = del[arg];
+        if (k === j) {
+            arr.splice((j - 1), 1);
+        } else continue;
+    }
+}
+    return arr;
 };
 
 // Do not edit below this line
