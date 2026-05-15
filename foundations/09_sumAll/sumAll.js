0da8467d8df0c7d5213fb51f31946301159ff5c8
Aydin Gurbanov <qurbanovaydin423@gmail.com>
2026-05-15 14:03:23 +0400

    Add the solution to the ex.09 that passes all the tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    if (a < 0 || b < 0) return 'ERROR';
+    if (Number.isNaN(a) || Number.isNaN(b)) return 'ERROR';
+    let arr = [a, b];
+    arr.sort(() => b - a);
+    let firstIndex = arr[0];
+    let lastIndex = arr[arr.length - 1];
+    for (let i = lastIndex - 1; i > firstIndex; i--) {
+        arr.push(i);
+    }
+    let sum = arr.reduce((x, y) => x + y, 0);
+    return sum;
 };
 
 // Do not edit below this line
